231a2df0071f5f95f4e77ea3476ca51cc083b37a
havababay <hava.babay@gmail.com>
2024-03-02 20:48:13 +0200

    Meet our team page - with mat-card and property binding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { PersonsListComponent } from './persons-list/persons-list.component';
 import { PersonFormComponent } from './person-form/person-form.component';
-import { PersonCardListComponent } from './person-card-list/person-card-list.component';
+import { TeamViewComponent } from './team-view/team-view.component';
 
 export const routes: Routes = [
     {path: '', component: PersonsListComponent},
     {path: 'person/:id', component : PersonFormComponent},
     {path: 'newperson', component : PersonFormComponent},
-    {path: 'team', component : PersonCardListComponent},
+    {path: 'team', component : TeamViewComponent},
 ];
