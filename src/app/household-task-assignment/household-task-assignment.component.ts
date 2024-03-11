5a735c4cc5344d6ee0cd9cd566286d80b278b330
havababay <hava.babay@gmail.com>
2024-03-11 06:24:00 +0200

    Generate random team in household task assignment

--- src/app/household-task-assignment/household-task-assignment.component.ts
+++ src/app/household-task-assignment/household-task-assignment.component.ts
@@ -6,13 +6,16 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { Person } from '../shared/model/person';
+import { PersonsService } from '../services/persons.service';
+import { PersonCardComponent } from '../person-card/person-card.component';
 
 @Component({
   selector: 'app-household-task-assignment',
   standalone: true,
   imports: [
     CommonModule, MatFormFieldModule, MatSelectModule, FormsModule,
-    MatButtonModule
+    MatButtonModule, PersonCardComponent
   ],
   templateUrl: './household-task-assignment.component.html',
   styleUrl: './household-task-assignment.component.css',
@@ -20,12 +23,23 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class HouseholdTaskAssignmentComponent implements OnInit { 
   allTasks : HouseholdTask[] = [];
+  allPerons : Person[] = [];
   selectedTask? : HouseholdTask;
+  randomTeam : Person[] = [];
 
   constructor(
-    private householdTasksService : HouseholdTasksService){}
+    private householdTasksService : HouseholdTasksService,
+    private personsService : PersonsService){}
 
   ngOnInit(): void {
-    this.allTasks = this.householdTasksService.list()
+    this.allTasks = this.householdTasksService.list();
+    this.allPerons = this.personsService.list();
+  }
+
+  generateRandomTeam() {
+    let shuffledPersons = 
+      [...this.allPerons].sort(() => Math.random() - 0.5);
+
+    this.randomTeam = shuffledPersons.splice(0,5);
   }
 }
